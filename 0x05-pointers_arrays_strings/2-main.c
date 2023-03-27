ba60d24d5a47bf82987b3c8675df76eb936f9f51
Starjakes <amehjacobe@gmail.com>
2023-03-27 07:26:17 -0700

    This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-main.c
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
  * main - check the code
@@ -12,7 +13,8 @@ int main(void)
 	int len;
 
 	str = "My first strlen!";
-	len = _strlen(str);
+	len = strlen(str);
+
 	printf("%d\n", len);
 	return (0);
 }
