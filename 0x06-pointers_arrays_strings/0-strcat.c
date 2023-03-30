7dca3471442c36a295186e23356fc6361d85776c
Starjakes <amehjacobe@gmail.com>
2023-03-29 21:09:01 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,11 +5,13 @@
 *@src:source string pointer
 *Return: pointer to destination string.
 */
-char *_strcat(char *dest, char *src);
+
+char *_strcat(char *dest, char *src)
 {
 int destlen = 0;
 int srclen = 0;
 int i;
+
 for (i = 0 : dest[i] = '\0' : i++)
 destlen++;
 for (i = 0 : src[i] = '\0' : i++)
