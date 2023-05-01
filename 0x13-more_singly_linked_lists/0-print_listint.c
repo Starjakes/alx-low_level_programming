7a31d2bba93a5c3af7ca231ce38abbba70e164cc
Starjakes <amehjacobe@gmail.com>
2023-04-30 17:53:52 -0700

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,11 +10,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t nodes = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		nodes++;
+		nnodes++;
 		printf("%d\n", h->n);
 		h = h->next;
 	}
-	return (nodes);
+	return (nnodes);
 }
